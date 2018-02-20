7bf668ca672ff4333e5d98afcc1216e8fadfd781
Mark Villudo <markanthony.villudo@gmail.com>
2018-02-20 15:23:10 +0800

    Update PermissionController.php

--- src/Controllers/PermissionController.php
+++ src/Controllers/PermissionController.php
@@ -13,7 +13,7 @@ class PermissionController extends Controller
 {
     public function __construct() 
     {
-        $this->middleware(['auth', 'isAdmin']);
+        $this->middleware(['auth']);
     }
 
     /**
