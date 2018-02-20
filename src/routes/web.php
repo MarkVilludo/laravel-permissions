ea3fb59d3ba362c5f1208cf3f3e2b627b7216e25
Mark Villudo <markanthony.villudo@gmail.com>
2018-02-20 15:31:13 +0800

    Update routes, include only resource routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,21 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('login');
-});
-Route::get('/home', function () {
-    return view('index');
-});
-Route::get('/logout', function () {
-    Auth::logout();
-    return view('login');
-});
-
 Route::resource('users', 'MarkVilludo\Permission\Controllers\UserController');
 
 Route::resource('roles', 'MarkVilludo\Permission\Controllers\RoleController');
 
 Route::resource('permissions', 'MarkVilludo\Permission\Controllers\PermissionController');
 
-Route::resource('posts', 'MarkVilludo\Permission\Controllers\PostController');
