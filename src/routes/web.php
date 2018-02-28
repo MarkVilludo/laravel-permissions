d2717f965bda3a14f8a5ed952ddc9181bf2c5e9a
Mark Villudo <markanthony.villudo@gmail.com>
2018-02-28 11:07:18 +0800

    Separate route for web and api list

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,15 @@ Route::resource('users', 'MarkVilludo\Permission\Controllers\UserController');
 
 Route::get('roles/createRoleApi', 'MarkVilludo\Permission\Controllers\RoleController@createRoleApi');
 Route::get('roles/createRoleWeb', 'MarkVilludo\Permission\Controllers\RoleController@createRoleWeb');
+
+
+Route::get('roles/webIndex', 'MarkVilludo\Permission\Controllers\RoleController@webIndex')->name('roles.webIndex');
+Route::get('roles/apiIndex', 'MarkVilludo\Permission\Controllers\RoleController@apiIndex')->name('roles.apiIndex');
+
 Route::resource('roles', 'MarkVilludo\Permission\Controllers\RoleController');
+
+Route::get('permissions/webIndex', 'MarkVilludo\Permission\Controllers\PermissionController@webIndex')->name('permissions.webIndex');
+Route::get('permissions/apiIndex', 'MarkVilludo\Permission\Controllers\PermissionController@apiIndex')->name('permissions.apiIndex');
 
 Route::resource('permissions', 'MarkVilludo\Permission\Controllers\PermissionController');
 
