5901380e1beace9c7c8a4f7bd325458f3449ccbb
Mark Villudo <markanthony.villudo@gmail.com>
2018-02-28 11:08:50 +0800

    Separate view api and web roles

--- views/roles/index.blade.php
+++ views/roles/index.blade.php
@@ -3,12 +3,14 @@
 @section('title', '| Roles')
 
 @section('content')
-
+<div class="pull-right" style="padding-right: 120px">
+    <a href="{{ route('users.index') }}" class="btn btn-default">Users</a>
+    <a href="{{ route('permissions.webIndex') }}" class="btn btn-default">Web Permissions</a>
+    <a href="{{ route('permissions.apiIndex') }}" class="btn btn-default">Api Permissions</a>
+</div>
 <div class="col-lg-10 col-lg-offset-1">
-    <h1><i class="fa fa-key"></i> Roles
-
-    <a href="{{ route('users.index') }}" class="btn btn-default pull-right">Users</a>
-    <a href="{{ route('permissions.index') }}" class="btn btn-default pull-right">Permissions</a></h1>
+    <h1><i class="fa fa-key"></i> {{$title}} </h1>
+   
     <hr>
     <div class="table-responsive">
         <table class="table table-bordered table-striped">
