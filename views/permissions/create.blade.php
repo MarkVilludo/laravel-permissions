9086921b2aac5dccc4a8305ac21b9d0445a521dc
Mark Villudo <markanthony.villudo@gmail.com>
2018-03-02 16:28:34 +0800

    Add module name

--- views/permissions/create.blade.php
+++ views/permissions/create.blade.php
@@ -4,38 +4,45 @@
 
 @section('content')
 
-<div class='col-lg-4 col-lg-offset-4'>
+ <div class='col-lg-4 col-lg-offset-4'>
 
-    {{-- @include ('errors.list') --}}
+                            {{-- @include ('errors.list') --}}
+                            
+                            {{ Form::open(array('url' => 'permissions')) }}
 
-    <h1><i class='fa fa-key'></i> Add Permission</h1>
-    <br>
+                            <div class="form-group">
+                                {{ Form::label('name', 'Name') }}
+                                {{ Form::text('name', '', array('class' => 'form-control')) }}
+                            </div>
+                            <br>
+                            <div class="form-group">
+                                {{ Form::label('module', 'Module Name') }}
+                                {{ Form::text('module', '', array('class' => 'form-control')) }}
+                            </div>
+                            <br>
+                            <h5><b>Assign Permission to Roles (Note: You may select API or Web ONLY.)</b></h5>
+                                <div class="row">
+                                    <div class='col-lg-6 form-group'>
+                                        <label>(WEB)</label> <br>
+                                        @foreach ($rolesWeb as $roleWeb)
+                                            {{ Form::checkbox('roles[]',  $roleWeb->id) }}
+                                            {{ Form::label($roleWeb->name, ucfirst($roleWeb->name)) }}<br>
 
-    {{ Form::open(array('url' => 'permissions')) }}
+                                        @endforeach
+                                    </div>
+                                    <div class='col-lg-6 form-group'>
+                                        <label>(API)</label> <br>
+                                        @foreach ($rolesApi as $roleApi)
+                                            {{ Form::checkbox('roles[]',  $roleApi->id) }}
+                                            {{ Form::label($roleApi->name, ucfirst($roleApi->name)) }}<br>
 
-    <div class="form-group">
-        {{ Form::label('name', 'Name') }}
-        {{ Form::text('name', '', array('class' => 'form-control')) }}
-    </div>
-    <br>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            <br>
+                            {{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
 
-    @if(!$roles->isEmpty())
+                            {{ Form::close() }}
 
-        <h4>Assign Permission to Roles</h4>
-
-        @foreach ($roles as $role) 
-            {{ Form::checkbox('roles[]',  $role->id ) }}
-            {{ Form::label($role->name, ucfirst($role->name)) }}<br>
-
-        @endforeach
-
-    @endif
-    
-    <br>
-    {{ Form::submit('Add', array('class' => 'btn btn-primary')) }}
-
-    {{ Form::close() }}
-
-</div>
-
+                        </div>
 @endsection
