e34eb832eb0f968c453f796d8404a677fa687f00
Mark Villudo <markanthony.villudo@gmail.com>
2018-03-02 16:29:02 +0800

    Add module name

--- views/permissions/edit.blade.php
+++ views/permissions/edit.blade.php
@@ -3,18 +3,20 @@
 @section('title', '| Edit Permission')
 
 @section('content')
-
+<!-- //Main content page. -->
 <div class='col-lg-4 col-lg-offset-4'>
 
     {{-- @include ('errors.list') --}}
 
-    <h1><i class='fa fa-key'></i> Edit {{$permission->name}}</h1>
-    <br>
     {{ Form::model($permission, array('route' => array('permissions.update', $permission->id), 'method' => 'PUT')) }}
 
     <div class="form-group">
         {{ Form::label('name', 'Permission Name') }}
         {{ Form::text('name', null, array('class' => 'form-control')) }}
+    </div>
+    <div class="form-group">
+        {{ Form::label('name', 'Module Name') }}
+        {{ Form::text('module', null, array('class' => 'form-control')) }}
     </div>
     <br>
     {{ Form::submit('Edit', array('class' => 'btn btn-primary')) }}
@@ -22,5 +24,6 @@
     {{ Form::close() }}
 
 </div>
+<!-- End main content page -->
 
 @endsection
